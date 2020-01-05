f60cd15ac4cf683700e3a7ea20afb4f6fe59d952
Bhushan Kolhe <kkbk58901@gmail.com>
2020-01-05 17:41:27 +0530

    Updated Color Palltte and added Hero content

--- src/Components/HeroArea/HeroArea.js
+++ src/Components/HeroArea/HeroArea.js
@@ -4,7 +4,15 @@ import './HeroArea.css';
 function HeroArea() {
     return (
         <div id="HeroArea">
-            
+            <div className="Container HeroContainer PrimaryFont">
+                <h1 className="Text-1 AlternateFont"> Hi, I am <span className="AlternateFont">Bhushan Kolhe</span>.</h1>
+                <h1 className="Text-2"> I like to build things.</h1>
+                <h1 className="Text-3">I am software developer based in India and I build things like  
+                    <span className="AlternateFont"> Websites</span>, <span className="AlternateFont"> 
+                    Applications</span> and <span className="AlternateFont">Games</span>. 
+                </h1>
+                <button className="GetInTouch-Btn">Get In Touch </button>
+            </div>
         </div>
     )
 }
