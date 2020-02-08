d9e27f008b8cb009bc158b80492b2ca33e896bc7
bhushan kolhe <kkbk58901@gmail.com>
2020-02-08 20:54:48 +0530

    Added basic google analytics

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { useState } from 'react';
 import Loader from './Components/Loader/Loader';
 import HomePage from './Components/HomePage/HomePage';
+import ReactGA from 'react-ga';
 import './App.css';
 
+function initializeAnalytics() {
+  const trackingId = "";
+  ReactGA.initialize(trackingId);
+  ReactGA.pageview('/HomePage');
+}
+
 function App() {
+  initializeAnalytics();
   const [ Loaded, setLoaded ] = useState(false);
   setTimeout( () => {
     setLoaded(true)
